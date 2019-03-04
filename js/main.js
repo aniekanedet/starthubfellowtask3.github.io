d4138bdd47ae469c1d016c0cb6b42620c78ce207
aniekanedet <anieaedet@gmail.com>
2019-03-04 17:27:18 +0100

    update files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,114 @@
 
 
-$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
-});
+// $('#carouselExample').on('slide.bs.carousel', function (e) {
+
+//     var $e = $(e.relatedTarget);
+//     var idx = $e.index();
+//     var itemsPerSlide = 4;
+//     var totalItems = $('.carousel-item').length;
+    
+//     if (idx >= totalItems-(itemsPerSlide-1)) {
+//         var it = itemsPerSlide - (totalItems - idx);
+//         for (var i=0; i<it; i++) {
+//             // append slides to end
+//             if (e.direction=="left") {
+//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+//             }
+//             else {
+//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+//             }
+//         }
+//     }
+// });
+
+
+
+// <section>
+//         <div class="container">
+//                 <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="5000">
+//                     <div class="carousel-inner row w-100 mx-auto" role="listbox">
+//                         <div class="carousel-item col-md-3 active">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                         <div class="carousel-item col-md-3">
+//                                 <div class="card">
+//                                         <img src="Assest/team-1.jpg" width="80%" class="card-img-top img-fluid d-flex justify-content-center align-self-center" alt="...">
+//                                         <div class="card-body">
+//                                         <h6 class="card-title text-center font-weight-bold">JACK CHRISTIAN</h6>
+//                                         <p class="card-text text-center"><small class="text-warning font-weight-bold">ELEVATOR</small></p>
+//                                         </div>
+//                                     </div>
+//                         </div>
+//                     </div>
+//                     <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+//                        <i class="fa fa-chevron-left fa-lg text-muted"></i>
+//                        <span class="sr-only">Previous</span>
+//                     </a>
+//                     <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+//                        <i class="fa fa-chevron-right fa-lg text-muted"></i>
+//                        <span class="sr-only">Next</span>
+//                     </a>
+//                 </div>
+//             </div>
+
+// </section>
